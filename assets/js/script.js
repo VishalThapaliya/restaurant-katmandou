1b3ffa37a9188beb77c9b097171979a6b9a8bfce
VishalThapaliya <vishal.thapaliya@gmail.com>
2023-06-23 14:29:50 +0200

    add reservation page

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,24 +24,25 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-  const scrollY = window.pageYOffset;
+// function scrollActive(){
+//   const scrollY = window.pageYOffset;
 
-  sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight
-      const sectionTop = current.offsetTop - 50;
-      sectionId = current.getAttribute('id')
+//   sections.forEach(current =>{
+//       const sectionHeight = current.offsetHeight
+//       const sectionTop = current.offsetTop - 50;
+//       sectionId = current.getAttribute('id');
+//       console.log('Toto', sectionId);
 
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-      }else{
-          document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-      }
-  })
-}
-window.addEventListener('scroll', scrollActive)
+//       if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//       }else{
+//           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//       }
+//   })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
@@ -114,26 +115,4 @@ function activeMenu() {
     menu.classList.remove('active-link');
     this.classList.add('active-link');
   });
-}
-
-
-/**=================== IMAGE SLIDER ================= */
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
